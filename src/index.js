677f7f91f186761a58849a55f7440ad6564e9af7
Martyna Skup <martyna.skup@yahoo.de>
2019-06-26 17:08:20 +0200

    Correct the typo in index.js: (delete the ; from render method)

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ class App extends React.Component {
     render() {
         return (
             <div className="border">
-                {this.renderContent()};
+                {this.renderContent()}
             </div>
         );
     }
