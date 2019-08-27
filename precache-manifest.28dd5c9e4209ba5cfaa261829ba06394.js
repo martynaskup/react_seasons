eeb9692be782a052d4363d92c76e40aa4f21e0f0
Martyna Skup <martyna.skup@yahoo.de>
2019-08-27 10:07:57 +0200

    Updates

---
+++ precache-manifest.28dd5c9e4209ba5cfaa261829ba06394.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5358b0144de8c8f37c2541f4202ef5b",
+    "url": "/react_seasons/index.html"
+  },
+  {
+    "revision": "b40598d435c16cc720d9",
+    "url": "/react_seasons/static/css/main.3b4acd10.chunk.css"
+  },
+  {
+    "revision": "607320d00d64b68e18da",
+    "url": "/react_seasons/static/js/2.2050718e.chunk.js"
+  },
+  {
+    "revision": "b40598d435c16cc720d9",
+    "url": "/react_seasons/static/js/main.6b2b8bfd.chunk.js"
+  },
+  {
+    "revision": "38742df24b3f97a596e8",
+    "url": "/react_seasons/static/js/runtime~main.da08049d.js"
+  }
+]);
