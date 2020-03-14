748d3ae48b6d042acd030893c81b34fa2c86d1c6
Martyna Skup <martyna.skup@yahoo.de>
2020-03-14 10:24:35 +0100

    Updates

---
+++ precache-manifest.98af3eed55bfaec09e0d3e564063f4ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67a59dc216d4ff51f7c8f2cab18108e1",
+    "url": "/react_seasons/index.html"
+  },
+  {
+    "revision": "962928c5a20975506e61",
+    "url": "/react_seasons/static/css/main.1db03b43.chunk.css"
+  },
+  {
+    "revision": "9a89f07e34ec17f4bae3",
+    "url": "/react_seasons/static/js/2.454ee9c6.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/react_seasons/static/js/2.454ee9c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "962928c5a20975506e61",
+    "url": "/react_seasons/static/js/main.6f5a8f1f.chunk.js"
+  },
+  {
+    "revision": "fa29ab2d0a2a473e174d",
+    "url": "/react_seasons/static/js/runtime-main.c95a7e90.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_seasons/precache-manifest.8e228badd945365ef02b5bb080776917.js"
+  "/react_seasons/precache-manifest.98af3eed55bfaec09e0d3e564063f4ee.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react_seasons/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
